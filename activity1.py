08ee460a1ff08f8987c32c9f1c5b9a1a76bb7589
ahnafnashit <ahnafnashitpro@gmail.com>
2025-10-27 17:23:19 +0600

    activity1

---
+++ activity1.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print("Hello, world!")
+
+# Printing my name
+print("My name is Nashit")
+
+# Printing a fun fact about myself
+print("I like to code and i also like watching soccer")
+
+#Performing and printing a simple calculation
+print("The result of 3+5 is:", 3 + 5)
