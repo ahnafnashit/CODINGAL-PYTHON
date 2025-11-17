650c365b06ca45ce5ef561b7509db56d1f8d3b8a
ahnafnashit <ahnafnashitpro@gmail.com>
2025-11-17 17:18:15 +0600

    activity3

---
+++ Conditional Statements/activity3.py
@@ -0,0 +1,8 @@
+i = int(input("enter a number :"))
+if(i<15):
+    print("i is smaller than 15")
+    print("I'm in if block")
+else:
+    print("i is greater than 15")
+    print("i', in else block")
+    print("Im not in if and not in else block")
