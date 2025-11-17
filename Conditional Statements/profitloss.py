a333a765aca07e67cfda9a163dec8228ea53e4ae
ahnafnashit <ahnafnashitpro@gmail.com>
2025-11-17 17:09:09 +0600

    profitloss

---
+++ Conditional Statements/profitloss.py
@@ -0,0 +1,10 @@
+actual_cost = float(input("Please enter the actual product price: "))
+
+sale_amount = float(input("Please enter the actual sales amount: "))
+
+if(sale_amount > actual_cost):
+    amount = sale_amount - actual_cost
+    print("Total profit = {0}".format(amount))
+else:
+    print("No profit!!!")
+    
